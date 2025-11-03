666d7f979cabd2bd75fa30643d9879d1daafa2db
Ritikyadav7830 <136908228+Ritikyadav7830@users.noreply.github.com>
2025-11-02 23:54:58 -0800

    Update app.js

--- app.js
+++ app.js
@@ -517,3 +517,12 @@ document.addEventListener("DOMContentLoaded", function() {
  
      setTimeout(hideLoading, 3000);
 }
+
+
+// Fix 100vh issue on mobile browsers
+function setVH() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setVH();
+window.addEventListener('resize', setVH);
